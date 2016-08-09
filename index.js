4aaf9afd1a2bb16fdc445ad10c7484383c3c0082
DejonGill <dejonjgill@gmail.com>
2016-08-09 14:42:29 -0700

    First Commit to Node

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const fs = require('fs');
+const Promise = require('Promise');
+
+
+const app = express();
+
+
+app.get('*', function(req, res) {
+
+  
+
+}).
+
+
+app.listen(3000, function() {
+  console.log('app listening on port 3000');
+});
