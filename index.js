9e99f500d3bd78dde912252ed78e78b24e716b0f
DejonGill <dejonjgill@gmail.com>
2016-08-09 23:52:23 -0700

    Added git ignore

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
+
 const fs = require('fs');
 const Promise = require('Promise');
 
@@ -8,11 +10,17 @@ const Promise = require('Promise');
 const app = express();
 
 
-app.get('*', function(req, res) {
+// app.get('*', function(req, res, next) {
 
-  
+//   // res.write;
 
-}).
+//   next();
+
+// }).
+
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
 
 app.listen(3000, function() {
